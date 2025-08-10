d63acbad0be83e2bce0009ff32531c31f36d5d14
Raskc <afraieda@gmail.com>
2025-08-11 00:49:16 +0200

    feat: lol

--- Framework/YippyFramework/Configuration/Cmdr/CommandsFunctions/position.lua
+++
@@ -1,9 +0,0 @@
-return function(_, player)
-    local character = player.Character
-
-    if not character or not character:FindFirstChild("HumanoidRootPart") then
-        return ""
-    end
-
-    return tostring(character.HumanoidRootPart.Position):gsub("%s", "")
-end
